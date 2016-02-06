01ab6d67381e942349cd58d5d3555250ecce2c5e
juice500ml <juice500ml@gmail.com>
2016-02-06 22:16:42 +0900

    config for WVGA.

--- desktop/src/com/blox/game/desktop/DesktopLauncher.java
+++ desktop/src/com/blox/game/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import com.blox.game.blox;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+		// WVGA (800x480)
+		config.width = 800;
+		config.height = 480;
+		
 		new LwjglApplication(new blox(), config);
 	}
 }
