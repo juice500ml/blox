6454a2c3ea840ac477e8e58a0c1dd9d2b1ba0ace
juice500ml <juice500ml@gmail.com>
2016-02-06 21:45:20 +0900

    Initial commit

---
+++ desktop/src/com/blox/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.blox.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.blox.game.blox;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new blox(), config);
+	}
+}
